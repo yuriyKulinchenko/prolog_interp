09e5d9baeae4842594792fa30318620f4e391585
Yuriy Kulinchenko <ykulinchenko@gmail.com>
2026-03-31 13:38:33 +0100

    Added ability to read from file directly, instead of from input string

--- main.cpp
+++ main.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
+#include <fstream>
 #include "helper.h"
 #include  "lexer.h"
 #include "parser.h"
 
+std::string read_file(const std::string& path) {
+    std::ifstream file(path, std::ios::binary | std::ios::ate);
+    if (!file) throw std::runtime_error("Failed to open file");
+
+    std::streamsize size = file.tellg();
+    file.seekg(0);
+
+    std::string buffer(size, '\0');
+    file.read(buffer.data(), size);
+
+    return buffer;
+}
+
 int main() {
-    std::string input = "p :- [a,b,c].";
+    std::string input = read_file("../source.txt");
     lexer lexer {input};
     auto vec = lexer.run();
     parser parser {vec};
     auto clauses = parser.run();
-    std::cout << clauses[0];
+    std::cout << clauses;
 }
